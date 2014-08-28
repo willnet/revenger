753779e06ce4a8ee227ae990c32169c16e950511
willnet <netwillnet@gmail.com>
2014-08-28 20:44:54 +0900

    Delete deprication warning of RSpec3

--- spec/fabrications_spec.rb
+++ spec/fabrications_spec.rb
@@ -3,7 +3,7 @@ Fabrication::Support.find_definitions
 describe 'Fabricate', solr: true do
   Fabrication.schematics.schematics.keys.each do |name|
     describe "(:#{name})" do
-      it { Fabricate(name).should be_valid }
+      it { expect(Fabricate(name)).to be_valid }
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,9 +20,9 @@ describe Post do
   describe '#body' do
     subject { Fabricate.build(:post) }
 
-    it { should accept_values_for(:body, "hoge", "!!!!!!", 'ほげほげ') }
+    it { is_expected.to accept_values_for(:body, "hoge", "!!!!!!", 'ほげほげ') }
     it "#{Post::MAX_BODY_LENGTH + 1}文字のとき invalid になること" do
-      should_not accept_values_for(:body, "", "a" * (Post::MAX_BODY_LENGTH + 1))
+      is_expected.not_to accept_values_for(:body, "", "a" * (Post::MAX_BODY_LENGTH + 1))
     end
   end
 
@@ -43,98 +43,98 @@ describe Post do
     context "user, {}" do
       subject { Post.search_by_solr(@user, {}).map(&:body) }
       it '作成日降順でオブジェクトの配列が返ること' do
-        should eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
+        is_expected.to eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
       end
     end
 
     context "user, {created_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {created_at: 'asc'}).map(&:body) }
       it '作成日昇順でオブジェクトの配列が返ること' do
-        should eq ['ふが24-24', 'ふが5-1', 'ふが4-3', 'ほげ3-2']
+        is_expected.to eq ['ふが24-24', 'ふが5-1', 'ふが4-3', 'ほげ3-2']
       end
     end
 
     context "user, {search: '', created_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {search: '', created_at: 'asc'}).map(&:body) }
       it '作成日昇順でオブジェクトの配列が返ること' do
-        should eq ['ふが24-24', 'ふが5-1', 'ふが4-3', 'ほげ3-2']
+        is_expected.to eq ['ふが24-24', 'ふが5-1', 'ふが4-3', 'ほげ3-2']
       end
     end
 
     context "user, {search: '', created_at: 'desc'}" do
       subject { Post.search_by_solr(@user, {search: '', created_at: 'desc'}).map(&:body) }
       it '作成日降順でオブジェクトの配列が返ること' do
-        should eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
+        is_expected.to eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
       end
     end
 
     context "user, {search: '', created_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {search: '', created_at: 'desc'}).map(&:body) }
       it '作成日降順でオブジェクトの配列が返ること' do
-        should eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
+        is_expected.to eq ['ほげ3-2', 'ふが4-3', 'ふが5-1', 'ふが24-24']
       end
     end
 
     context "user, {modified_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {modified_at: 'asc'}).map(&:body) }
       it '更新日昇順でオブジェクトの配列が返ること' do
-        should eq ['ふが24-24', 'ふが4-3', 'ほげ3-2', 'ふが5-1']
+        is_expected.to eq ['ふが24-24', 'ふが4-3', 'ほげ3-2', 'ふが5-1']
       end
     end
 
     context "user, {search: '', modified_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {search: '', modified_at: 'asc'}).map(&:body) }
       it '更新日昇順でオブジェクトの配列が返ること' do
-        should eq ['ふが24-24', 'ふが4-3', 'ほげ3-2', 'ふが5-1']
+        is_expected.to eq ['ふが24-24', 'ふが4-3', 'ほげ3-2', 'ふが5-1']
       end
     end
 
     context "user, {modified_at: 'desc'}" do
       subject { Post.search_by_solr(@user, {modified_at: 'desc'}).map(&:body) }
       it '更新日降順でオブジェクトの配列が返ること' do
-        should eq ['ふが5-1', 'ほげ3-2', 'ふが4-3', 'ふが24-24']
+        is_expected.to eq ['ふが5-1', 'ほげ3-2', 'ふが4-3', 'ふが24-24']
       end
     end
 
     context "user, {search: '', modified_at: 'desc'}" do
       subject { Post.search_by_solr(@user, {search: '', modified_at: 'desc'}).map(&:body) }
       it '更新日昇順でオブジェクトの配列が返ること' do
-        should eq ['ふが5-1', 'ほげ3-2', 'ふが4-3', 'ふが24-24']
+        is_expected.to eq ['ふが5-1', 'ほげ3-2', 'ふが4-3', 'ふが24-24']
       end
     end
 
     context "{search: 'ふが'}" do
       subject { Post.search_by_solr(@user, {search: 'ふが'}).map(&:body) }
       it '"ふが"を含むオブジェクトの配列が作成日降順で返ること' do
-        should eq ['ふが4-3', 'ふが5-1', 'ふが24-24']
+        is_expected.to eq ['ふが4-3', 'ふが5-1', 'ふが24-24']
       end
     end
 
     context "{search: 'ふが', created_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {search: 'ふが', created_at: 'asc'}).map(&:body) }
       it '"ふが"を含むオブジェクトの配列が作成日昇順で返ること' do
-        should eq ['ふが24-24', 'ふが5-1', 'ふが4-3']
+        is_expected.to eq ['ふが24-24', 'ふが5-1', 'ふが4-3']
       end
     end
 
     context "{search: 'ふが', created_at: 'desc'}" do
       subject { Post.search_by_solr(@user, {search: 'ふが', created_at: 'desc'}).map(&:body) }
       it '"ふが"を含むオブジェクトの配列が作成日降順で返ること' do
-        should eq ['ふが4-3', 'ふが5-1', 'ふが24-24']
+        is_expected.to eq ['ふが4-3', 'ふが5-1', 'ふが24-24']
       end
     end
 
     context "{search: 'ふが', modified_at: 'asc'}" do
       subject { Post.search_by_solr(@user, {search: 'ふが', modified_at: 'asc'}).map(&:body) }
       it '"ふが"を含むオブジェクトの配列が更新日昇順で返ること' do
-        should eq ['ふが24-24', 'ふが4-3', 'ふが5-1']
+        is_expected.to eq ['ふが24-24', 'ふが4-3', 'ふが5-1']
       end
     end
 
     context "{search: 'ふが', modified_at: 'desc'}" do
       subject { Post.search_by_solr(@user, {search: 'ふが', modified_at: 'desc'}).map(&:body) }
       it '"ふが"を含むオブジェクトの配列が更新日降順で返ること' do
-        should eq ['ふが5-1', 'ふが4-3', 'ふが24-24']
+        is_expected.to eq ['ふが5-1', 'ふが4-3', 'ふが24-24']
       end
     end
 
