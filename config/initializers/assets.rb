6ba7fe1c923eb0b98c49af0e5e084e23da52185e
willnet <netwillnet@gmail.com>
2014-12-01 15:02:06 +0900

    Bump up rails and ruby

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,4 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "co
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings.png glyphicons-halflings-white.png )
