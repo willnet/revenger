8e2f25e5c82b189633ad6beadb4f5f5f5a4ed043
willnet <netwillnet@gmail.com>
2020-09-01 17:47:21 +0900

    本番環境のメール配信をpostfixからsendgridに変更
    将来的にクラウド環境で本番運用することを見越して、sendgridを利用することにした

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'revenger.in', protocol: 'https://' }
-  config.action_mailer.smtp_settings = { host: 'localhost', enable_starttls_auto: false }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY'],
+    raise_delivery_errors: true
+  }
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
 end
