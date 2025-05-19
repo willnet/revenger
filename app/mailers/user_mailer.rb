cb5ac5b7412f2b73e4112864e4232e864f580c0e
Shinichi Maeshima <netwillnet@gmail.com>
2025-05-19 22:45:50 +0900

    Rails6.1でテストを通るようにした

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class UserMailer < ActionMailer::Base
   default from: "noreply@revenger.in"
-  add_template_helper(ApplicationHelper)
+  helper ApplicationHelper
 
   def assignment(user)
     @user = user
