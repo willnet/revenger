d2037db03a67de55eda0d8e834df2e01fec143ca
Shinichi Maeshima <netwillnet@gmail.com>
2026-01-27 16:59:06 +0900

    Refactor: Improve CreatePostContext spec

--- spec/contexts/create_post_context_spec.rb
+++ spec/contexts/create_post_context_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe CreatePostContext do
   describe '.call' do
     let(:post) { Post.new }
+    let!(:user) { Fabricate(:user, default_duration: 7) }
 
     before do
-      user = OpenStruct.new(id: 1, default_duration: 7)
       params = ActionController::Parameters.new(post: { body: '投稿本文テスト' })
       Timecop.freeze
       CreatePostContext.call(user, post, params)
@@ -13,7 +13,7 @@ describe CreatePostContext do
     end
 
     it 'post に適切にパラメータが設定されていること' do
-      expect(post.user_id).to eq 1
+      expect(post.user).to eq user
       expect(post.body).to eq '投稿本文テスト'
       expect(post.modified_at).to be_the_same_time_as Time.zone.now
       expect(post.duration).to eq 7
