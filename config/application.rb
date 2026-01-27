9997bcf823004032a5366eb5d52be1c074519917
Shinichi Maeshima <netwillnet@gmail.com>
2026-01-27 13:15:07 +0900

    refactor: Update application configuration for Rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,13 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Revenger
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
+    config.load_defaults 7.1
     config.eager_load_paths += [Rails.root.join('lib').to_s]
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Tokyo'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     config.generators do |g|
@@ -32,6 +19,7 @@ module Revenger
     end
   end
 end
+
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
