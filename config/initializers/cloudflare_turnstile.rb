ec67e7aea2aaec556fd2cb4665b4ca4de2622d5c
Shinichi Maeshima <netwillnet@gmail.com>
2026-02-06 15:21:01 +0900

    turnstileのkeyにCredentialsを使うようにした

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -2,11 +2,12 @@ RailsCloudflareTurnstile.configure do |c|
   # In development/test, use mock if env vars not set
   # In production, require real credentials
   if Rails.env.production?
-    c.site_key = ENV.fetch('CLOUDFLARE_TURNSTILE_SITE_KEY')
-    c.secret_key = ENV.fetch('CLOUDFLARE_TURNSTILE_SECRET_KEY')
+    c.site_key = Rails.application.credentials.dig(:cloudflare_turnstile, :site_key)
+    c.secret_key = Rails.application.credentials.dig(:cloudflare_turnstile, :secret_key)
   else
-    c.site_key = ENV.fetch('CLOUDFLARE_TURNSTILE_SITE_KEY', '1x00000000000000000000AA')
-    c.secret_key = ENV.fetch('CLOUDFLARE_TURNSTILE_SECRET_KEY', '1x0000000000000000000000000000000AA')
+    c.site_key = 'dummy_site_key' # Dummy key for development/test
+    c.secret_key = 'dummy_secret_key' # Dummy key for development/test
   end
   c.fail_open = true
+  c.enabled = !Rails.env.local? && !ENV["SECRET_KEY_BASE_DUMMY"]
 end
