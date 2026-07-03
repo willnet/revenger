d337d36e818fe2cc2f763f1e2373f9e71ba0552a
Shinichi Maeshima <netwillnet@gmail.com>
2026-07-03 17:37:50 +0900

    使われていないテストヘルパーを削除した

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -1,9 +1,3 @@
-def set_subdomain(subdomain)
-  Capybara.default_host = "#{subdomain}.example.com" # for Rack::Test
-  Capybara.app_host = "http://#{subdomain}.revenger.te:3000" # for another drivers
-  I18n.locale = subdomain
-end
-
 def sign_in(email, password)
   visit login_path
   fill_in "user_email", with: email
